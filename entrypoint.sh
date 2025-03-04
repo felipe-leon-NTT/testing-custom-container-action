285d4ddff23592d024eba839f92b639dab8b8095
Felipe <162121819+felipe-leon-NTT@users.noreply.github.com>
2025-03-04 16:40:14 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,8 +2,8 @@
 
 if [ $2 = 'polite' ]; then
     echo "Hello, Mr. $1"
-    echo "Hello, Mr. $1" >> $GITHUB_OUTPUT
+    echo "msg=Hello, Mr. $1" >> $GITHUB_OUTPUT
 else
     echo "Hi, $1"
-    echo "Hi, $1" >> $GITHUB_OUTPUT
+    echo "msg=Hi, $1" >> $GITHUB_OUTPUT
 fi
